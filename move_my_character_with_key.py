9ff4f1d37cd006ff385739bbf8fd15b47cd6ea7c
JUHYEONSU <1434gus@tukorea.ac.kr>
2025-09-25 23:03:11 +0900

    좌우 이동 완료
    눌렀을 때 horizontal 변수 추가

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -8,13 +8,13 @@ open_canvas(window_width, window_height)
 ground = load_image('TUK_GROUND.png')
 character = load_image('sonic-sprite.png')
 frame = 0
-x = 0
-dir = 0
+x = 400
+horizontal = 0
 speed = 0.1
 
 def handle_events():
     global running
-    global x
+    global x, horizontal
 
     events = get_events()
 
@@ -25,9 +25,14 @@ def handle_events():
             if event.key == SDLK_ESCAPE:
                 running = False
             elif event.key == SDLK_RIGHT:
-                x += 10
+                horizontal += 1
             elif event.key == SDLK_LEFT:
-                x -= 10
+                horizontal -= 1
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                horizontal -= 1
+            elif event.key == SDLK_LEFT:
+                horizontal += 1
 
 ch_width = 29
 ch_height = 40
@@ -35,16 +40,16 @@ ch_height = 40
 while running:
     clear_canvas()
     ground.draw(window_width/2,window_height/2)
-    # character.clip_draw(0,0,30,50,x,90)
 
     clear_canvas()
     ground.draw(window_width / 2, window_height / 2)
-    character.clip_draw(86 + 32 * frame, 447, ch_width, ch_height, 400 + x, 300, ch_width / 29 * 300, ch_height / 40 * 400)
+    character.clip_draw(86 + 32 * frame, 447, ch_width, ch_height, x, 300, ch_width / 29 * 60, ch_height / 40 * 80)
+
     handle_events()
     update_canvas()
     frame = (frame + 1) % 4
+    x += horizontal * 7
     delay(speed)
-
 
 
 close_canvas()
