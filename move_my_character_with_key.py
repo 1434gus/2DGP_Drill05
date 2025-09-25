cd98aaf9ac873a577bd73ba8aa419ee6233873c3
JUHYEONSU <1434gus@tukorea.ac.kr>
2025-09-25 23:08:55 +0900

    상하 이동 완료
    vertical 변수 추가

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -9,12 +9,14 @@ ground = load_image('TUK_GROUND.png')
 character = load_image('sonic-sprite.png')
 frame = 0
 x = 400
+y = 300
 horizontal = 0
-speed = 0.1
+vertical = 0
+speed = 0.05
 
 def handle_events():
     global running
-    global x, horizontal
+    global x, y, horizontal, vertical
 
     events = get_events()
 
@@ -28,11 +30,19 @@ def handle_events():
                 horizontal += 1
             elif event.key == SDLK_LEFT:
                 horizontal -= 1
+            elif event.key == SDLK_UP:
+                vertical += 1
+            elif event.key == SDLK_DOWN:
+                vertical -= 1
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
                 horizontal -= 1
             elif event.key == SDLK_LEFT:
                 horizontal += 1
+            elif event.key == SDLK_UP:
+                vertical -= 1
+            elif event.key == SDLK_DOWN:
+                vertical += 1
 
 ch_width = 29
 ch_height = 40
@@ -43,12 +53,14 @@ while running:
 
     clear_canvas()
     ground.draw(window_width / 2, window_height / 2)
-    character.clip_draw(86 + 32 * frame, 447, ch_width, ch_height, x, 300, ch_width / 29 * 60, ch_height / 40 * 80)
+    character.clip_draw(86 + 32 * frame, 447, ch_width, ch_height, x, y, ch_width / 29 * 60, ch_height / 40 * 80)
 
     handle_events()
     update_canvas()
+
     frame = (frame + 1) % 4
     x += horizontal * 7
+    y += vertical * 7
     delay(speed)
 
 
